cc0b322b46588786e3bbf4bd652db4da5c5a55a5
Bulat <bulat20031006@gmail.com>
2022-12-19 22:54:20 +0300

    desktop minor changes

---
+++ APPLICATION/Services/AuthorizationService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPLICATION.Services
+{
+    public class UserNotFoundException : Exception { }
+    public class AuthenticationFail : Exception { }
+    public class TooManyAuthorizationAttempts : Exception { }
+
+    public class AuthorizationService
+    {
+        private const int _AUTHORIZATION_ATTEMPTS_THRESHOLD_MINUTES = 1;
+        private const int _MAX_AUTHORIZATION_ATTEMPTS = 3;
+        private readonly GIBDDEntities _db;
+
+        public AuthorizationService()
+        {
+            _db = new GIBDDEntities();
+        }
+        public AuthorizationService(GIBDDEntities db)
+        {
+            _db = db;
+        }
+        public Users Auth(string login, string password)
+        {
+            var authorizationAttempts = GetAuthorizationAttempts(login);
+            if (authorizationAttempts.Count >= _MAX_AUTHORIZATION_ATTEMPTS)
+            {
+                throw new TooManyAuthorizationAttempts();
+            }
+
+            RegisterAuthorizationAttempt(login);
+
+            var user = Identify(login);
+            if (user == null)
+            {
+                throw new UserNotFoundException();
+            }
+            if (Authenticate(user, password))
+            {
+                return user;
+            }
+            throw new AuthenticationFail();
+
+        }
+
+        private Users Identify(string login)
+        {
+            var user = _db.Users.Where(x => x.Login == login).FirstOrDefault();
+            return user;
+        }
+
+        private bool Authenticate(Users user, string password)
+        {
+            return user.Password == password;
+        }
+
+        private string GetPersonIdentity()
+        {
+            return Environment.MachineName.ToString();
+        }
+
+        private void RegisterAuthorizationAttempt(string login)
+        {
+            _db.AuthorizationAttempts.Add(
+                new AuthorizationAttempts()
+                {
+                    Login = login,
+                    PersonIdentity = GetPersonIdentity(),
+                    AttemptDate = DateTime.Now
+                }
+            );
+            _db.SaveChanges();
+        }
+
+        private List<AuthorizationAttempts> GetAuthorizationAttempts(string login)
+        {
+            var sinceDate = DateTime.Now;
+            sinceDate = sinceDate.AddMinutes(-_AUTHORIZATION_ATTEMPTS_THRESHOLD_MINUTES);
+
+            return _db.AuthorizationAttempts.Where(x =>x.Login == login).Where(x => x.AttemptDate > sinceDate).ToList();
+        }
+    }
+}
---
+++ DESKTOP_APP/Views/AuthorizationView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using APPLICATION.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DESKTOP_APP.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationView.xaml
+    /// </summary>
+    public partial class AuthorizationView : Window
+    {
+        private readonly AuthorizationService _authorizationService;
+        public AuthorizationView()
+        {
+            InitializeComponent();
+            _authorizationService = new AuthorizationService();
+        }
+
+        private void authorizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var login = loginTextBox.Text;
+            var password = passwordBox.Password;
+
+            try
+            {
+                var user = _authorizationService.Auth(login, password);
+                MessageBox.Show("Успешная авторизация!");
+            }
+            catch (UserNotFoundException)
+            {
+                MessageBox.Show("Пользователь не найден");
+            }
+            catch (TooManyAuthorizationAttempts)
+            {
+                MessageBox.Show("Превышено число попыток. Попробуйте позже");
+            }
+            catch (AuthenticationFail)
+            {
+                MessageBox.Show("Неверный пароль");
+            }
+        }
+    }
+}
